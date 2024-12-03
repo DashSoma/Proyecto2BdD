4e17fd7e0398157b187eccd2fb9d8726385e470c
dash <daobandoso@est.utn.ac.cr>
2024-12-03 14:44:06 -0600

    Clase ProveedoresDTO
    Se crea la clase dto con los metodos y atributos final

---
+++ Modelo/Proveedor/ProveedorDTO.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.Proveedor;
+
+/**
+ *
+ * @author dashs
+ */
+public class ProveedorDTO {
+    private final int id;
+    private final String nombre;
+    private final String contacto;
+    private final String direccion;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+   
+    public ProveedorDTO(int id, String nombre, String contacto, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.direccion = direccion;
+    }
+}
