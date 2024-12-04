ce6b16d5299129b3d72df8fb238f90bf7f19e988
Daniel-sql123 <dadelgadoal@est.utn.ac.cr>
2024-12-03 18:34:20 -0600

    Se crea el paquete User
    Se crean las clases User y UserDTO en el paquete de User, con sus atributos y metodos

---
+++ User/User.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+
+/**
+ *
+ * @author DaniTini
+ */
+public class User {
+    private int id;
+    private String nombre;
+    private String correo;
+    private String pass;
+
+    public User() {
+    }
+
+    public User(int id, String nombre, String correo, String pass) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+  
+}
---
+++ User/UserDTO.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Usuario
+ */
+public class UserDTO {
+    private final int id;
+    private final String nombre;
+    private final String correo;
+    private final String pass;
+
+    public UserDTO(int id, String nombre, String correo, String pass) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+    
+    
+    
+}
